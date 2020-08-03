af5e16d6e9f1e9ef9f2021ecaa4d123063142fa2
Monteirolnx <monteirolnx@gmail.com>
2020-08-02 22:38:01 -0300

    Create / List

---
+++ SampleWebApps/RPDemoApp/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPDemoApp.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IFoodData _foodData;
+        private readonly IOrderData _orderData;
+
+        public List<SelectListItem> FoodItems { get; set; }
+
+        [BindProperty]
+        public OrderModel Order { get; set; }
+
+        public CreateModel(IFoodData foodData, IOrderData orderData)
+        {
+            _foodData = foodData;
+            _orderData = orderData;
+        }
+
+        public async Task OnGet()
+        {
+            List<FoodModel> food = await _foodData.GetFood();
+
+            FoodItems = new List<SelectListItem>();
+
+            food.ForEach(f =>
+            {
+                FoodItems.Add(new SelectListItem { Value = f.Id.ToString(), Text = f.Title });
+            });
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            List<FoodModel> food = await _foodData.GetFood();
+            Order.Total = Order.Quantity * food.Where(x => x.Id == Order.FoodId).First().Price;
+
+            int id = await _orderData.CreateOrder(Order);
+
+            return RedirectToPage("./Display", new { Id = id});
+        }
+    }
+}
